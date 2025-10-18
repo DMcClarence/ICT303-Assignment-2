05837f48521a533e5a027ffd51d3ab0f3568a28c
Dylan McClarence <61504185+DMcClarence@users.noreply.github.com>
2025-10-18 17:35:13 +0800

    Start Custom CNN

---
+++ CNN.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+
+    @staticmethod
+    def forward(self, x):
+        return NotImplemented
