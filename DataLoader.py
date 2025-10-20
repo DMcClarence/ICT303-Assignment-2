7a95b417d3aee6632f9246cd9ea1421ffa59f040
Dylan McClarence <34085068@student.murdoch.edu.au>
2025-10-20 12:50:06 +0800

    Change dataloader to work for fire dataset

--- DataLoader.py
+++ DataLoader.py
@@ -11,5 +11,12 @@ class DataLoader:
 
   def load(self, transform, batch_size=10, shuffle=True, workers=2):
     dataset = datasets.ImageFolder(root=self.root, transform=transform)
-    dataset = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=workers)
-    return dataset
+    train_size = int(len(dataset) * 0.8) # 80%
+    valid_size = int(len(dataset) * 0.1) # 10%
+    test_size = int(len(dataset) * 0.1)  # 10%
+    training_data, validation_data, test_data = torch.utils.data.random_split(dataset, [train_size, valid_size, test_size])
+
+    train_loader = torch.utils.data.DataLoader(training_data, batch_size, shuffle=shuffle, num_workers=workers)
+    valid_loader = torch.utils.data.DataLoader(validation_data, batch_size, shuffle=shuffle, num_workers=workers)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=workers)
+    return train_loader, valid_loader, test_loader
